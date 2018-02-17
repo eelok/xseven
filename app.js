7454ae183816262bb527f0ea0199c275d0ab9cd4
wassertim <tim@digitalband.ru>
2018-02-17 19:47:17 +0100

    ref #11 send email

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var port = 4242;
@@ -12,6 +13,32 @@ var server = app.listen(port, function() {
   console.log('Magic happens on port ' + port);
 });
 
-app.post('/receive-message', function(request, response) {
-    console.log(request.body);
+function sendEmail(contactData) {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+               user: process.env.XSEVEN_EMAIL,
+               pass: process.env.XSEVEN_PASSWORD
+           }
+       });
+
+    const mailOptions = {
+        from: contactData.email,
+        to: process.env.XSEVEN_EMAIL,
+        subject: `Message from: ${contactData.name}, ${contactData.email}`,
+        html: contactData.message
+    };
+      
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(info);
+        }
+     });
+}
+
+app.post('/receive-message', function(request, response) {    
+    sendEmail(request.body);  
+    response.sendfile("app/contact.html");
 });
